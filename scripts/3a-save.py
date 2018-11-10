cdea5663e2764604cc68a1cb48e6ee3dfaba1898
root <root@localhost.localdomain>
2018-11-10 23:15:36 +0100

    3a pas de gestion d'exceptions

--- scripts/3a-save.py
+++ scripts/3a-save.py
@@ -11,12 +11,23 @@ import os
 import sys
 import signal
 
-src = "/tmp/pyscripts/B2-Python/scripts/bidon"
 filename = "bidon"
+src = "/tmp/pyscripts/B2-Python/scripts/" + filename
 dst = "/tmp/pyscripts/B2-Python/scripts/data"
 
+# Fonction gestion signal
+def quit(*args):
+  try:
+    os.remove(src + ".tar.gz")
+    sys.stdout.write("Sauvegarde annulé.")
+  except:
+    sys.stdout.write("Au revoir !")
+  exit(0)
+
+signal.signal(signal.SIGINT, quit)
+
 # On archive le répertoire choisi
-shutil.make_archive(src, 'gztar')
+shutil.make_archive(src, 'gztar', src)
 filename += ".tar.gz"
 
 # L'archive existe-t-elle déjà dans le dossier data ?
@@ -24,15 +35,22 @@ isAlreadyHere = False
 for file in os.listdir(dst):
   if file == filename:
     isAlreadyHere = True
-    print("Fichier déjà présent")
+    sys.stdout.write("Fichier déjà présent. Je vérifie si il y a une différence entre les fichiers. \n")
     break;
 
 # Si l'archive est déjà présente, le contenu est-t-il différent ?
+isFileDifferent = False
 if isAlreadyHere:
   new_file = gzip.open(src + ".tar.gz").read()
   current_file = gzip.open(dst + "/"  + filename).read()
   if new_file == current_file:
-    print("Fichiers identiques ! ")
+    sys.stdout.write("Pas de différences. On annule la sauvegarde. \n")
   else:
-    print("Déplacement du fichier")
-    shutil.move(src + ".tar.gz", dst + "/" + filename)
+    isFileDifferent = True
+    sys.stdout.write("Différences détectés. On déplace l'archive. \n")
+
+# Déplacement du fichier
+if (isAlreadyHere and isFileDifferent) or isAlreadyHere == False:
+  shutil.move(src + ".tar.gz", dst + "/" + filename)
+
+os.remove(src + ".tar.gz")
